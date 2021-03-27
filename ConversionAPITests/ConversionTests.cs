83e05f82163789a15b0c292c3f3ac104076eaa0b
Jaco Beylefeld <jj.beylefeld@gmail.com>
2021-03-27 12:51:58 +0200

    initial commit

---
+++ ConversionAPITests/ConversionTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ConversionAPITests
+{
+    public class ConversionTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ ConversionAPITests/UnitsNetLearningTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using UnitsNet;
+
+namespace ConversionAPITests
+{
+    public class UnitsNetLearningTests
+    {
+        [Fact]
+        public void convertCelsiusToFahrenheit()
+        {
+            Assert.Equal(32, Math.Round(Temperature.FromDegreesCelsius(0).DegreesFahrenheit,2));
+        }
+
+        [Fact]
+        public void convertFahrenheitToCelcius()
+        {
+            Assert.Equal(0, Math.Round(Temperature.FromDegreesFahrenheit(32).DegreesCelsius,2));
+        }
+
+        [Fact]
+        public void convertKFToPound()
+        {
+            Assert.Equal(2.20462, Math.Round(Mass.FromKilograms(1).Pounds,5));
+        }
+
+        [Fact]
+        public void convertPoundToKG()
+        {
+            Assert.Equal(1, Math.Round(Mass.FromPounds(2.20462).Kilograms,5));
+        }
+    }
+}
