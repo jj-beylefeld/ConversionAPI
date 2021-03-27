2347b6aaa53aab1a094653345d7356b5538ea209
Jaco Beylefeld <jj.beylefeld@gmail.com>
2021-03-27 16:41:30 +0200

    implement temperature request

---
+++ ConversionAPI/Classes/Implementation/TemperatureConverterRequest.cs
@@ -0,0 +1,24 @@
+﻿using ConversionAPI.Classes.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConversionAPI.Classes.Implementation
+{
+    public class TemperatureConverterRequest : IConverterRequest
+    {
+        public string fromType { get ; set ; }
+        public double fromValue { get ; set ; }
+        public string toType { get ; set ; }
+
+        public void validate()
+        {
+            if (!Enum.TryParse(fromType, true, out SupportedTypes.Temperature thefromType))
+                throw new ArgumentOutOfRangeException(nameof(fromType));
+
+            if (!Enum.TryParse(toType, true, out SupportedTypes.Temperature thetoType))
+                throw new ArgumentOutOfRangeException(nameof(toType));
+        }
+    }
+}
---
+++ ConversionAPITests/TemperatureConverterRequestTests.cs
@@ -0,0 +1,48 @@
+﻿using ConversionAPI.Classes.Interfaces;
+using ConversionAPI.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ConversionAPI.Classes.Implementation;
+
+namespace ConversionAPITests
+{
+    public class TemperatureConverterRequestTests
+    {
+        [Fact]
+        public void InvalidFromTypeThrowsArgumentOutOfRangeException()
+        {
+            IConverterRequest converterRequest = new TemperatureConverterRequest { 
+                fromType = "Notsupported"
+                , fromValue = 0.00
+                , toType = "celsius"
+            };
+            Assert.Throws<ArgumentOutOfRangeException>(() => converterRequest.validate());
+        }
+
+        [Fact]
+        public void InvaliToTypeThrowsArgumentOutOfRangeException()
+        {
+            IConverterRequest converterRequest = new TemperatureConverterRequest
+            {
+                fromType = "celsius"
+                , fromValue = 0.00
+                , toType = "Notsupported"
+            };
+            Assert.Throws<ArgumentOutOfRangeException>(() => converterRequest.validate());
+        }
+
+        [Fact]
+        public void ValidPasses()
+        {
+            IConverterRequest converterRequest = new TemperatureConverterRequest
+            {
+                fromType = "celsius"
+                , fromValue = 0.00
+                , toType = "fahrenheit"
+            };
+            converterRequest.validate();
+        }
+    }
+}
